eb9587f3813294ccff443fc1830156b4026a9f54
LeHuyVuu <lehuyvuok@gmail.com>
2025-03-13 15:25:11 +0700

    modify text

--- Movie App/src/app/components/MediaList/MediaList.jsx
+++ Movie App/src/app/components/MediaList/MediaList.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { MovieCard } from "./MovieCard";
 import useFetch from "../../hooks/useFetch";
-
+import "./MediaList.css";
 const MediaList = ({ TABS, Title }) => {
 
     const [activeTabId, setActiveTabId] = useState(TABS[0]?.id);
@@ -21,37 +21,37 @@ const MediaList = ({ TABS, Title }) => {
 
     return (
         <>
-<div className="bg-black text-white p-4">
-<div className=" justify-center bg-white p-4 rounded-sm  flex items-center gap-3 ">
-      <div>QUICK BOOK</div>
-      {/* Select Theater */}
-      <select className="px-3 py-2 border rounded text-black font-semibold">
-        <option>Select Theater</option>
-      </select>
+            <div className="bg-black  p-4">
+                <div className=" justify-center bg-white p-4 rounded-sm  flex items-center gap-3 ">
+                    <div>QUICK BOOK</div>
+                    {/* Select Theater */}
+                    <select className="px-3 py-2 border rounded text-black font-semibold">
+                        <option>Select Theater</option>
+                    </select>
 
-      {/* Select Movie */}
-      <select className="px-3 py-2 border rounded text-black font-semibold">
-        <option>Select Movie</option>
-      </select>
+                    {/* Select Movie */}
+                    <select className="px-3 py-2 border rounded text-black font-semibold">
+                        <option>Select Movie</option>
+                    </select>
 
-      {/* Select Date */}
-      <select className="px-3 py-2 border rounded text-black font-semibold">
-        <option>Select Date</option>
-      </select>
+                    {/* Select Date */}
+                    <select className="px-3 py-2 border rounded text-black font-semibold">
+                        <option>Select Date</option>
+                    </select>
 
-     
 
-      {/* Booking Button */}
-      <button className="bg-black text-white font-bold px-4 py-2 rounded-lg hover:bg-red-400">
-        BOOKING
-      </button>
-      </div>
-    </div>
+
+                    {/* Booking Button */}
+                    <button className="bg-black text-white font-bold px-4 py-2 rounded-lg hover:bg-red-400">
+                        BOOKING
+                    </button>
+                </div>
+            </div>
+
             <div className="px-8 py-10 bg-black text-white">
-
                 <div className="flex flex-col items-start gap-4">
                     {/* Phần tiêu đề */}
-                    <p className="text-2xl font-bold">{Title}</p>
+                    <p className="text-4xl font-extrabold w-full text-center text-sparkle">{Title}</p>
                     {/* Phần danh sách */}
                     {/* <ul className="flex items-center justify-start space-x-2 rounded-full px-2 py-1 w-full sm:w-3/4 md:w-2/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5 bg-black">
                     {TABS.map((tabItem) => (
@@ -82,7 +82,41 @@ const MediaList = ({ TABS, Title }) => {
                         />
                     ))}
                 </div>
+            </div>
+            <div className="px-8 py-10 bg-black text-white">
+                <div className="flex flex-col items-start gap-4">
+                    {/* Phần tiêu đề */}
+                    <p className="text-4xl font-extrabold w-full text-center text-sparkle">UP COMING</p>
+                    {/* Phần danh sách */}
+                    {/* <ul className="flex items-center justify-start space-x-2 rounded-full px-2 py-1 w-full sm:w-3/4 md:w-2/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5 bg-black">
+                    {TABS.map((tabItem) => (
+                        <li
+                            onClick={() => handleTabChange(tabItem.id)}
+                            key={tabItem.id}
+                            className={`px-4 sm:px-6 lg:px-8 py-2 rounded-full cursor-pointer transition duration-300 ease-in-out text-xs sm:text-sm md:text-base lg:text-lg text-center whitespace-nowrap ${tabItem.id === localStorage.getItem("activeTabId")
+                                ? "bg-white text-black hover:bg-gray-300 shadow-md" // Tab được chọn
+                                : "bg-gray-800 text-white hover:bg-gray-600" // Tab không được chọn
+                                }`}
+                        >
+                            {tabItem.name}
+                        </li>
 
+                    ))}
+                </ul> */}
+                </div>
+                <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-fr p-4">
+                    {mediaList.map((media) => (
+                        <MovieCard
+                            id={media.id}
+                            key={media.id}
+                            title={media.title}
+                            releaseDate={media.release_date}
+                            poster={media.poster_path}
+                            point={media.vote_average}
+                            mediaType={media.media_type}
+                        />
+                    ))}
+                </div>
             </div>
         </>
     );

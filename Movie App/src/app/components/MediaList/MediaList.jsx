9a78d145b4de5cbd94d5bbe287b13b5e0d9a5823
LeHuyVuu <lehuyvuok@gmail.com>
2025-03-13 15:49:19 +0700

    a

--- Movie App/src/app/components/MediaList/MediaList.jsx
+++ Movie App/src/app/components/MediaList/MediaList.jsx
@@ -90,8 +90,7 @@ const MediaList = ({ TABS, Title }) => {
                     {/* Phần danh sách */}
                     {/* <ul className="flex items-center justify-start space-x-2 rounded-full px-2 py-1 w-full sm:w-3/4 md:w-2/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5 bg-black">
                     {TABS.map((tabItem) => (
-                        <li
-                            onClick={() => handleTabChange(tabItem.id)}
+                        <li onClick={() => handleTabChange(tabItem.id)}
                             key={tabItem.id}
                             className={`px-4 sm:px-6 lg:px-8 py-2 rounded-full cursor-pointer transition duration-300 ease-in-out text-xs sm:text-sm md:text-base lg:text-lg text-center whitespace-nowrap ${tabItem.id === localStorage.getItem("activeTabId")
                                 ? "bg-white text-black hover:bg-gray-300 shadow-md" // Tab được chọn
